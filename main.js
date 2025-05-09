c4648ee6b430b37e997a3cf5584e91bc0559853d
sashkka1 <84565833+sashkka1@users.noreply.github.com>
2025-05-09 15:45:31 +0300

    Update main.js

--- main.js
+++ main.js
@@ -971,7 +971,7 @@ function themeChange(color){
 
 
 document.addEventListener('DOMContentLoaded', () => { // первый заход и разложение сохраненных значений
-    console.log('Try 31');
+    console.log('Try 32');
 
 
     
